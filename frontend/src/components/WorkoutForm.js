8372895f2bfebe2afd597bb0ce8902b8f1cd3432
Ahmad Ramin Soleyman kheyl <sramin963@gmail.com>
2023-11-28 19:04:38 +0300

    add icon from material symbol and date-fns package

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -5,7 +5,7 @@ const WorkoutForm = () => {
     const [load, setLoad] = useState('')
     const [reps, setReps] = useState('')
     const [error, setError] = useState(null)
-   
+    const [emptyFields, setEmptyFields] = useState([])
     const handleSubmit = async (e) => {
         e.preventDefault()
         
@@ -22,12 +22,14 @@ const WorkoutForm = () => {
 
         if(!response.ok) {
             setError(json.error)
+            setEmptyFields(json.emptyField)
         }
         if(response.ok) {
             setTitle('')
             setLoad('')
             setReps('')
             setError(null)
+            setEmptyFields([])
         }
     }
     
@@ -40,6 +42,7 @@ const WorkoutForm = () => {
                 type='text' 
                 onChange={(e)=> setTitle(e.target.value)}
                 value={title}
+                className={emptyFields.includes('title') ? 'error' : ''}
             />
 
             <label>Loud(in kg):</label>
@@ -47,6 +50,7 @@ const WorkoutForm = () => {
                 type='number' 
                 onChange={(e)=> setLoad(e.target.value)}
                 value={load}
+                className={emptyFields.includes('load') ? 'error' : ''}
             />
 
             <label>Reps:</label>
@@ -54,6 +58,7 @@ const WorkoutForm = () => {
                 type='number' 
                 onChange={(e)=> setReps(e.target.value)}
                 value={reps}
+                className={emptyFields.includes('reps') ? 'error' : ''}
             />
 
             <button >Add Workout</button>
